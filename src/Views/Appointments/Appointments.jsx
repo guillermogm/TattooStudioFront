0461060c39ceab57ab1cf6c2deaf0a87cd4876d8
guillermogm <guillermogm009@gmail.com>
2024-07-28 14:48:50 +0200

    feat: Select Component

--- src/Views/Appointments/Appointments.jsx
+++ src/Views/Appointments/Appointments.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { deleteAppointmentById, getProfileAppointments } from '../../Services/apiCalls'
+import "./Appointments.css"
 
 export const Appointments = () => {
     const [error, setError] = useState("")
     const [appointments, setAppointments] = useState([])
     let [update, setUpdate] = useState(0)
+    const [editing, setEditing] = useState(false)
+    const [newAppointment, setNewAppointment] = useState({
+        userId: "",
+        serviceId: "",
+        date: ""
+    });
     useEffect(() => {
         const fullToken = JSON.parse(localStorage.getItem("fullToken"))
         const token = fullToken.token
@@ -27,12 +34,30 @@ export const Appointments = () => {
         const res = await deleteAppointmentById(id, token)
         if (res.success) {
             setError("")
-            setUpdate(++ update)
-        }else{
+            setUpdate(++update)
+        } else {
             setError("Error deleting Appointment.")
         }
     }
 
+    function editingButtonHandler() {
+        setEditing(!editing)
+    }
+    const inputHandler = (e) => {
+
+        if (e.target.value === "Elige el servicio") {
+            console.log("You cannot pass")
+            return
+        }
+        console.log(e.target.value)
+        setNewAppointment({
+            ...newAppointment,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const todayFullTimeString = new Date()
+        .toISOString()
+        .slice(0, new Date().toISOString().lastIndexOf(":"));
     return (
         <>
             <h1 className="text-center mt-5 mb-5">Appointments</h1>
@@ -48,7 +73,7 @@ export const Appointments = () => {
                     </thead>
                     <tbody>
                         {appointments.length && appointments.map((appointment) => {
-                            let date= new Date (appointment.appointmentDate)
+                            let date = new Date(appointment.appointmentDate)
                             return (
                                 <tr key={appointment.id} className="text-center">
                                     <th scope="row">{appointment.id}</th>
@@ -62,9 +87,27 @@ export const Appointments = () => {
                         })}
                     </tbody>
                 </table>
-            </div>
-            <div className="text-center">
-                <input type="button" className="btn btn-primary" value="Create" />
+                <div className="row d-flex justify-content-center text-center">
+                    <div className="col-2">
+                        <input
+                            type="datetime-local"
+                            min={todayFullTimeString}
+                            value={newAppointment.date}
+                            name="date"
+                            onChange={(e) => inputHandler(e)}
+                            className={editing ? "mb-4 form-control" : "hidden"}
+                        />
+                    </div>
+                    <div className="col-2">
+                        {/* className={editing ? "mb-4 form-control" : "hidden"} */}
+                    </div>
+                </div>
+                <div className="text-center">
+                    <input type="button" name="Create" className="btn btn-primary" value={editing ? "Cancel" : "Create"} onClick={editingButtonHandler} />
+                </div>
+                <div className="text-center mt-2">
+                    <input type="button" name="Save" value="Save" className={editing ? "btn btn-primary" : "hidden"} />
+                </div>
             </div>
             <h1 className="text-center mt-5 mb-5">{error}</h1>
         </>
